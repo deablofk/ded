eab11fb05951ee4ae03d50e7a01c33aca80c5144
deablofk <deablofk@gmail.com>
2025-02-02 15:59:38 -0400

    fix: quit command not using close function

--- src/main/java/dev/cwby/commands/Quit.java
+++ src/main/java/dev/cwby/commands/Quit.java
@@ -1,12 +1,17 @@
 package dev.cwby.commands;
 
 import dev.cwby.graphics.Engine;
+import dev.cwby.graphics.SkiaRenderer;
 
 public class Quit implements ICommand {
 
     @Override
     public boolean run(String[] args) {
-        Engine.setShouldClose(true);
+        if (SkiaRenderer.currentNode.father == null) {
+            Engine.setShouldClose(true);
+        } else {
+            SkiaRenderer.currentNode.close();
+        }
         return true;
     }
 }
